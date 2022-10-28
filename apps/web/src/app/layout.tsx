aa0b69db60f75360391c926902aa2604af206798
adamw-af <adamw@accessfintech.com>
2022-10-28 15:15:55 +0100

    init

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,14 @@
+import './global.css';
+
+export default function RootLayout({ children }: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
